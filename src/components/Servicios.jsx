941e4167016f092db284d8047192869647d54b4c
Jhonny Iriarte <iriarte.condori.jhonny25@gamil.com>
2023-10-16 23:53:17 -0400

    nueva version

--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -5,7 +5,7 @@ import { service } from "../slides";
 const Services = () => {
   return (
     <section id="servicios" className=" bg-teal-950 pt-16 pb-16 ">
-      <div className="container mx-auto text-center">
+      <div className="container mx-auto text-center ">
         <div className="flex justify-center  gap-2 p-2">
         < FaInstagram className="w-8 h-8 text-cyan-400  " />
         <p className=" text-white font-light">@puntodefe</p>
@@ -17,7 +17,7 @@ const Services = () => {
           comprometidos biblicamente en cada una de nuestras actividades, visita nuestro perfil de instagram
         </p>
       </div>
-      <div className="container mx-auto mt-10">
+      <div className="container mx-auto mt-10 p-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {service.map((skill, index) => (
             <div key={index} className=" flex flex-col  items-center  bg-white rounded-lg p-2 shadow-md">
